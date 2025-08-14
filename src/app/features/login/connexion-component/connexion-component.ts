f996aa1fd854f58e3b66d4033c9f75ec47671165
Deku5005 <dolooumar60@gmail.com>
2025-08-14 17:23:40 +0000

    modification

--- src/app/features/login/connexion-component/connexion-component.ts
+++ src/app/features/login/connexion-component/connexion-component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-import {Router, RouterLink, RouterOutlet} from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {AuthService} from '../../../services/auth-service';
+import { AuthService } from '../../../services/auth-service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-connexion-component',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, FormsModule, HttpClientModule],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    FormsModule,
+    HttpClientModule,
+    MatSnackBarModule
+  ],
   templateUrl: './connexion-component.html',
-  styleUrl: './connexion-component.css'
+  styleUrls: ['./connexion-component.css'] 
 })
 export class ConnexionComponent {
   formData = {
@@ -17,36 +24,44 @@ export class ConnexionComponent {
     password: ''
   };
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
-onSubmit() {
-  this.authService.login(this.formData.email, this.formData.password)
-    .subscribe({
-      next: (res) => {
-        localStorage.setItem('token', res.token);
-        localStorage.setItem('id', res.id.toString());
-        localStorage.setItem('roles', JSON.stringify(res.roles));
-        console.log('Connexion réussie', res);
+  onSubmit() {
+    this.authService.login(this.formData.email, this.formData.password)
+      .subscribe({
+        next: (res) => {
+          localStorage.setItem('token', res.token);
+          localStorage.setItem('id', res.id.toString());
+          localStorage.setItem('roles', JSON.stringify(res.roles));
+          console.log('Connexion réussie', res);
 
-        // Redirection selon le rôle
-        if (res.roles.includes('Administrateur')) {
-          this.router.navigate(['/AdminDashboard']);
-        } else if (res.roles.includes('Gestionnaire')) {
-          this.router.navigate(['/dashboardContributeur']);
-        } else if (res.roles.includes('Contributeur')) {
-          this.router.navigate(['/dashboardContributeur']);
-        } else {
           
-          this.router.navigate(['/Connexion']);
+          this.snackBar.open(' Connexion réussie ! Bienvenue ', '', {
+            duration: 4000,
+            panelClass: ['custom-snackbar']
+          });
+
+          // Redirection selon le rôle
+          if (res.roles.includes('Administrateur')) {
+            this.router.navigate(['/AdminDashboard']);
+          } else if (res.roles.includes('Gestionnaire')) {
+            this.router.navigate(['/dashboardContributeur']);
+          } else if (res.roles.includes('Contributeur')) {
+            this.router.navigate(['/dashboardContributeur']);
+          } else {
+            this.router.navigate(['/Connexion']);
+          }
+        },
+        error: (err) => {
+          this.snackBar.open('❌ Identifiants incorrects', '', {
+            duration: 3000,
+            panelClass: ['error-snackbar']
+          });
         }
-      },
-      error: (err) => {
-        alert(err.error.message || "Identifiants incorrects");
-      }
-    });
-}
-
-
-
-
+      });
+  }
 }
